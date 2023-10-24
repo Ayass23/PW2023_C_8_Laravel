a7e1c66eaba7bd6704f562759ac1608acdce38eb
Ayass23 <andreasmargono.23@gmail.com>
2023-10-25 06:08:00 +0700

    fix styling

--- resources/views/Pembayaran.blade.php
+++ resources/views/Pembayaran.blade.php
@@ -78,7 +78,7 @@
                                     <label for="exampleFormControlInput1">No. Whatsapp</label>
                                     <input type="text" class="form-control mt-2" id="exampleFormControlInput1" placeholder="081xxxxxxxx"required >
                                 </div>
-                                <button class="btn btn-success mt-2" style="width:100%;">Bayar</button>
+                                <button class="btn btn-success mt-2" data-bs-toggle="modal" data-bs-target="#exampleModalCenter" style="width:100%;">Bayar</button>
                             </form>
                         </div>
                     </div>
@@ -133,7 +133,28 @@
             
         </div>
     </div>
+
     
+    <div class="modal " id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header bg-success">
+              <h5 class="modal-title text-white" id="exampleModalCenterTitle">Selamat !!!</h5>
+              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    Selamat Pembelian tiket telah berhasil, mohon segera menyelesaikan pembayaran dan detail tiket akan dikirim melalui email. See you on Event !! 
+                </p>
+            </div>
+            <div class="modal-footer ">
+              <button type="button" class="btn btn-outline-success" onclick="window.location='{{ url('homepage') }}'">Done</button>
+            </div>
+          </div>
+        </div>
+    </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
